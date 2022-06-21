ae4fd2ff82237dbed5d0244a82afe1f4e9948af1
ManhNV <manhnv@miichisoft.net>
2022-06-21 15:59:32 +0700

    Assets

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -1,9 +1,20 @@
 import Link from "next/link";
+import Image from 'next/image';
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+);
 
 export default function FirstPost() {
     return (
       <>
         <h1>First Post</h1>
+        <YourComponent />
         <h2>
           <Link href="/">
             <a>Back to home</a>
