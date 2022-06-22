e888c3398b8ad7dc7a523724c84976df2c206c64
ManhNV <manhnv@miichisoft.net>
2022-06-22 09:50:25 +0700

    Layout Component

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -1,31 +1,20 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
-import Script from "next/script";
-
-const YourComponent = () => (
-  <Image
-    src="/images/profile.jpg" // Route of the image file
-    height={144} // Desired size with correct aspect ratio
-    width={144} // Desired size with correct aspect ratio
-    alt="Your Name"
-  />
-);
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
         <title>First Post</title>
       </Head>
 
       <h1>First Post</h1>
-      <YourComponent />
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
-    </>
+    </Layout>
   );
 }
