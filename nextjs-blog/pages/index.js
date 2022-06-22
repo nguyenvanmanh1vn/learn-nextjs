798c4f8df10870dd22567b237eadf9e7b6bf4d50
ManhNV <manhnv@miichisoft.net>
2022-06-22 17:58:52 +0700

    Pre-rendering and Data Fetching

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,7 +1,6 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import Link from "next/link";
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
   return (
@@ -12,13 +11,10 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <Link href="https://nextjs.org/learn">
-            <a>our Next.js tutorial</a>
-          </Link>
-          .)
+          (This is a sample website - you’ll be building a site like this in{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
     </Layout>
-  );
+  )
 }
