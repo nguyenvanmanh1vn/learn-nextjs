56f7e9fd07168442fd82048f7525947cfcd5db8c
ManhNV <manhnv@miichisoft.net>
2022-06-23 10:52:10 +0700

    getStaticProps Details

--- nextjs-blog/lib/posts.js
+++ nextjs-blog/lib/posts.js
@@ -1,37 +1,16 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+// import someDatabaseSDK from 'someDatabaseSDK'
 
-const postsDirectory = path.join(process.cwd(), 'posts');
+// export async function getSortedPostsData() {
+//   // Instead of the file system,
+//   // fetch post data from an external API endpoint
+//   const res = await fetch('..');
+//   return res.json();
+// }
 
-export function getSortedPostsData() {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
+// const databaseClient = someDatabaseSDK.createClient(...)
 
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data,
-    };
-  });
-  // Sort posts by date
-  return allPostsData.sort(({ date: a }, { date: b }) => {
-    if (a < b) {
-      return 1;
-    } else if (a > b) {
-      return -1;
-    } else {
-      return 0;
-    }
-  });
-}
+// export async function getSortedPostsData() {
+//   // Instead of the file system,
+//   // fetch post data from a database
+//   return databaseClient.query('SELECT posts...')
+// }
